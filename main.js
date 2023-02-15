a960456bdc241b5be27a008ba4df9f84a9725bb2
Ounaiss3 <124074239+Ounaiss3@users.noreply.github.com>
2023-02-15 18:32:53 +0100

    Third write a function that plays 5 rounds, keeps score and report winner or loser

--- main.js
+++ main.js
@@ -7,12 +7,15 @@ return selection [Math.floor(Math.random()*selection.length)];
      
 }
 
+
+
 const selection = ["Rock", "Paper", "Scissors"];
 
 
 // Second write a fonction that plays 1 single round of RPS
 function playRound(playerSelection, computerSelection) {
   // your code here!
+
   if (computerSelection === "Paper") {
     console.log("Computer wins") 
   }
@@ -26,4 +29,24 @@ function playRound(playerSelection, computerSelection) {
  
 const playerSelection = "Rock";
 const computerSelection = getComputerChoice(selection);
-console.log(playRound(playerSelection, computerSelection));
+
+//function getPlayerChoice() {
+    
+    //let choice = prompt("Choose Rock, Paper or Scissors")
+      //  return choice
+//}
+
+//Third write a function that plays 5 rounds, keeps score and report winner or loser
+ function game(playRound) {
+    for (let i = 0; i < 5; i++) {
+        if (computerSelection === "Paper") {
+            console.log("Computer wins") 
+          }
+          else if (computerSelection === "Scissors" ) {
+            console.log("Player wins")
+          }
+          else {
+            console.log("It's a draw")
+          }
+     }
+ }
